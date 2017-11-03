b8bfcc91eedecbeb4724caf85ea305e3435724f7
Ben Gantz <bengantz@gmail.com>
2017-11-03 20:18:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function add(1, 2) {
-  return 1 + 2
+function add(a, b) {
+  return a + b
 }
-function subtract(1, 2) {
-  return 1 - 2
+function subtract(a, b) {
+  return a - b
 }
-function multiply(1, 2) {
-  return 1 * 2
+function multiply(a, b) {
+  return a * b
 }
-function divide(1, 2) {
-  return 1 / 2
+function divide(a, b) {
+  return a / b
 }
 function inc(n) {
   return n++
